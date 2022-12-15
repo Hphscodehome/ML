859e216f1d273d558e568204be9ec1079e7c93cd
Hphscodehome <2213113587@qq.com>
2022-12-15 10:09:55 +0800

    new

---
+++ test.py
@@ -0,0 +1,61 @@
+#输入库
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm,metrics,datasets
+from zipfile import ZipFile
+import pandas as pd
+""" #输入数据
+digits=datasets.load_digits()
+digits_images_target=list(zip(digits.images,digits.target))
+#画出数据(在图像识别中用到)
+#枚举前四个数据集，并画出图像enumerate((digits_images_target[:4]))
+for (i,(images,label)) in enumerate((digits_images_target[:4])):
+    plt.subplot(4,4,i+1)
+    plt.imshow(images,cmap=plt.cm.gray_r,interpolation='nearest')
+    plt.axis('off')
+    plt.title('Training:%d'%label)
+#训练模型 """
+myzip=ZipFile("train.csv.zip")
+fp=myzip.open("train.csv")
+train_data=pd.read_csv(fp,header=None)
+fp.close()
+myzip.close()
+myzip=ZipFile("test.csv.zip")
+fp=myzip.open("test.csv")
+test_data=pd.read_csv(fp,header=None)
+fp.close()
+myzip.close()
+X_train=train_data.iloc[:,:39]
+Y_train = train_data.iloc[:,40]
+X_test = test_data
+clf=svm.SVC(gamma=0.01)
+clf.fit(X_train,Y_train)
+predicted=clf.predict(X_test)
+print(X_train[:20])
+print(predicted[:20])
+""" data=digits.images
+n_samples=len(data)
+data=data.reshape((n_samples,-1))
+X=data[:n_samples//2]
+y=digits.target[:n_samples//2]
+clf=svm.SVC(gamma=0.01)
+clf.fit(X,y)
+#print(clf)
+#预测
+expect=digits.target[n_samples//4:]
+predicted=clf.predict(data[n_samples//4:])
+print('Classification report for classifier %s:\n%s\n'%(clf,metrics.classification_report(expect,predicted)))
+print('clf_report Confusion Matrix:\n%s'%metrics.confusion_matrix(expect,predicted))
+fpr, tpr, thresholds = metrics.roc_curve(expect,predicted, pos_label=2)
+
+print('Classification ACU report for classifier %s:\n%s\n'%(clf,metrics.auc(fpr, tpr)))
+#print (predicted)
+#确认验证集的输入和预测值
+images_and_predictions = list(zip(digits.images[n_samples // 4:], predicted))
+for (i,(expect,predicted)) in enumerate((images_and_predictions[:12])):
+    plt.subplot(4,4,i+5)
+    plt.subplots_adjust(left=0.2,right=1.0,bottom=0.1,top=1.5)
+    plt.imshow(expect,cmap=plt.cm.gray_r,interpolation='nearest')
+    plt.axis('off')
+    plt.title('prediction:%d'%predicted)
+plt.show() """
--- troubledata.py
+++ troubledata.py
@@ -1,4 +1,83 @@
 #读取数据
-
+from zipfile import ZipFile
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from matplotlib import pyplot as plt
+myzip=ZipFile("train.csv.zip")
+fp=myzip.open("train.csv")
+train_data=pd.read_csv(fp,header=None)
+fp.close()
+myzip.close()
+myzip=ZipFile("test.csv.zip")
+fp=myzip.open("test.csv")
+test_data=pd.read_csv(fp,header=None)
+fp.close()
+myzip.close()
 #处理数据
-#输出数据
+import torch as t
+from torch import nn
+from torch import optim
+from torch.autograd import Variable
+import torch.utils.data as Data
+#from IPython import display
+class classifer(nn.Module):
+    def __init__(self):
+        super(classifer, self).__init__()
+        self.class_col = nn.Sequential(
+            nn.Linear(40,16),
+            nn.ReLU(),
+            nn.Linear(16,16),
+            nn.ReLU(),
+            nn.Linear(16,23),
+        )
+    def forward(self, x):
+        out = self.class_col(x)
+        return out
+from torch import optim
+model = classifer() # 实例化模型
+loss_fn = nn.CrossEntropyLoss() # 定义损失函数
+optimiser = optim.SGD(params=model.parameters(), lr=0.05) # 定义优化器
+from torch.autograd import Variable
+import torch.utils.data as Data
+X_train=t.Tensor(np.array(train_data.iloc[:200,:39]))
+Y_train = t.Tensor(np.array(train_data.iloc[:200,40]))
+X_test = t.Tensor(np.array(test_data))
+#X_train=t.Tensor(train_data.iloc[:,:39])
+#Y_train = t.Tensor(train_data.iloc[:,40])
+#X_test = t.Tensor(test_data)
+# 使用batch训练
+torch_dataset = Data.TensorDataset(X_train, Y_train) # 合并训练数据和目标数据
+MINIBATCH_SIZE = 25
+loader = Data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=MINIBATCH_SIZE,
+    shuffle=True,
+    num_workers=10           # set multi-work num read data
+)
+loss_list = []
+plt.style.use('ggplot')
+for epoch in range(70):
+    for step, (batch_x, batch_y) in enumerate(loader):
+        optimiser.zero_grad() # 梯度清零
+        out = model(batch_x) # 前向传播
+        loss = loss_fn(out, batch_y) # 计算损失
+        loss.backward() # 反向传播
+        optimiser.step() # 随机梯度下降
+    loss_list.append(loss)
+    if epoch%10==0:
+        outputs_train = model(X_train)
+        _, predicted_train = t.max(outputs_train, 1)
+        outputs_test = model(X_test)
+        _, predicted_test = t.max(outputs_test, 1)
+        #predicted_test就是训练出的结果
+    
+        # 同时画出训练集和测试的效果
+        #display.clear_output(wait=True)
+        #fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(13,7))
+        #axes[0].scatter(X_train[:,0].numpy(),X_train[:,1].numpy(),c=predicted_train)
+        #axes[0].set_xlabel('train')
+        #axes[1].scatter(X_test[:,0].numpy(),X_test[:,1].numpy(),c=predicted_test)
+        #axes[1].set_xlabel('test')
+        #display.display(fig)
+print(predicted_test)
